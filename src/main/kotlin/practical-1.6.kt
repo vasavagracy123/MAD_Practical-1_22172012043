36d7e1bb6142b0e69954bdeaf0f64bd5cf25f47c
student <student@gnu.ac.in>
2023-07-21 12:26:17 +0530

    hey

--- src/main/kotlin/practical-1.6.kt
+++ src/main/kotlin/practical-1.6.kt
@@ -1,19 +1,34 @@
-fun ArithmeticOperation(operator:Char,operand1:Double,operand2:Double):Double{
-    return when (operator){
-        '+' -> operand1+operand2
-        '-' -> operand1-operand2
-        '*' -> operand1*operand2
-        '/' -> operand1/operand2
-        else -> throw IllegalArgumentException("Error: Invalid operator.")
-    }
+fun main()
+{
+    var a = 34
+    var b = 34
+    var add = add(a,b)
+    println("Addition of ${a} and ${b} is : "+add)
+
+    var sub = sub(a,b)
+    println("Substractiob of ${a} and ${b} is : "+sub)
+
+    var mul = mul(a,b)
+    println("Multiplicatin of ${a} and ${b} is : "+mul)
+
+    var div = div(a,b)
+    println("Division of ${a} and ${b} is : "+div)
 }
-fun main(){
-    print("Enter the first number:")
-    val operand1 = readLine()?.trim()!!.toDouble()
-    print("Enter the second number:")
-    val operand2 = readLine()?.trim()!!.toDouble()
-    print("Enter the operator(+,-,*,/):")
-    val operator = readLine()!![0]
-    val result = ArithmeticOperation(operator,operand1,operand2)
-    println("Result:$result")
+
+
+fun add(a:Int,b:Int):Int
+{
+    return a + b
+}
+
+fun sub(a:Int,b:Int):Int
+{
+    return a - b
+}
+fun mul(a:Int,b:Int):Int
+{
+    return a * b
+}
+fun div(a:Int,b:Int):Int {
+    return a / b
 }
